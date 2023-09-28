d8212edd054c984a66a8a839f4fdc6dff90c7244
Orynbassar1ly4s <131473714+Orynbassar1ly4s@users.noreply.github.com>
2023-09-28 13:19:58 +0600

    Add files via upload

---
+++ lab_1.py
@@ -0,0 +1,69 @@
+# lab1 Orynbassaruly Ilyas SIS-2134
+#Task 1
+#1.1
+# print("4 8 15 16 23 42")
+#1.2
+# print("4 \n8 \n15 \n16 \n23 \n42")
+#1.3
+# n=int(input())
+# print(n+1)
+# print(n+2)
+#1.4
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=a+b+c
+# print(d)
+#1.5
+# a=int(input())
+# V=a**3
+# TSA=6*a**2
+# print(V, TSA)
+
+#task2
+#2.1
+# n=int(input())
+# k=int(input())
+# print(k//n)
+# print(k%n)
+#2.2
+#a=int(input())
+# n1=a//1000
+# n2=(a//100)%10
+# n3=(a%100)//10
+# n4=a%10
+# print('The digit in the thousands position is', n1)
+# print('The digit in the hundreds position is', n2)
+# print('The digit in the tens position is', n3)
+# print('The digit in the position of units is', n4)
+#2.3
+# a=int(input())
+# print((a+1)//2)
+#2.4
+# input_number = int(input("Enter a number: "))
+# result = input_number << 1
+# if result == 0:
+#     print("Warning: The result of << is zero")
+# else:
+#     print(f"The result of << is {result}")
+#2.5
+# num1 = float(input("Please enter the first number: "))
+# num2 = float(input("Please enter the second number: "))
+# operation = input("Please choose the operation (+, -, *, /) : ")
+# if operation == "+":
+#     result = num1 + num2
+# elif operation == "-":
+#     result = num1 - num2
+# elif operation == "*":
+#     result = num1 * num2
+# elif operation == "/":
+#     if num2 != 0:
+#         result = num1 / num2
+#     else:
+#         print("Error: Division by zero is not allowed.")
+#         result = None
+# else:
+#     print("Invalid operation choice.")
+#     result = None
+# if result is not None:
+#     print(f"{num1} {operation} {num2} = {result:.3f}")
